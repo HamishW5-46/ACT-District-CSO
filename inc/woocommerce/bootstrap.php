cc8f1a3aa33de0635bba1479d2641d1d9463fb74
Hamish Wright <Hamishw905@outlook.com>
2026-08-25 22:29:24 +1000

    Remove duplicate product description heading

--- inc/woocommerce/bootstrap.php
+++ inc/woocommerce/bootstrap.php
@@ -7,3 +7,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 require_once get_stylesheet_directory() . '/inc/woocommerce/assets.php';
 require_once get_stylesheet_directory() . '/inc/woocommerce/shop-filters.php';
 require_once get_stylesheet_directory() . '/inc/woocommerce/account-auth.php';
+require_once get_stylesheet_directory() . '/inc/woocommerce/product-tabs.php';
