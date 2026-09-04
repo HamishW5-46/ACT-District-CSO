bf0bea10e6a48931ec5e6515a56d78179e3ef2a7
Hamish Wright <hamishw905@outlook.com>
2026-09-04 22:11:26 +1000

    Update include handle in functions.php

--- functions.php
+++ functions.php
@@ -10,11 +10,11 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Site-wide behaviour lives in MU plugins so changing themes does not silently
  * remove privacy controls, archive rules, or WooCommerce features.
  */
-$act_district_cso_child_includes = array(
+$act_district_cso_includes = array(
     '/inc/bootstrap.php',
     '/inc/site-branding.php',
 );
 
-foreach ( $act_district_cso_child_includes as $act_district_cso_child_include ) {
-    require_once get_stylesheet_directory() . $act_district_cso_child_include;
+foreach ( $act_district_cso_includes as $act_district_cso_include ) {
+    require_once get_stylesheet_directory() . $act_district_cso_include;
 }
